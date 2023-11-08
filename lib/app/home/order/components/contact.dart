5f3b1b947f8f013c2b19d92ac1b85e70c6da6581
Svitlana Kravchenko <svitlanak@madappgang.com>
2023-11-08 19:31:22 +0200

    Clickable phone number, show contacts after order details

--- lib/app/home/order/components/contact.dart
+++ lib/app/home/order/components/contact.dart
@@ -1,9 +1,10 @@
+import 'dart:developer';
+
 import 'package:agro/model/model_order/model_order.dart';
 import 'package:agro/ui/text/read_text.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-import '../../../../ui/buttons/b_style.dart';
 import '../../../../ui/buttons/b_transparent_scalable_button.dart';
 
 class ContactScreen extends StatelessWidget {
@@ -14,41 +15,76 @@ class ContactScreen extends StatelessWidget {
   @override
   Widget build(BuildContext c) {
     return Container(
-      height: 440,
       color: Colors.white,
       child: Column(
         children: [
-          Padding(
-              padding: const EdgeInsets.only(top: 15),
-              child: Container(decoration: BoxDecoration(color: const Color(0xffEDEDED), borderRadius: BorderRadius.circular(3)), height: 10, width: 100)),
-          readText(text: 'Контакти фермера', color: Colors.black, fontWeight: FontWeight.w600, size: 22, padding: const EdgeInsets.only(top: 25, bottom: 30)),
+          // Padding(
+          //     padding: const EdgeInsets.only(top: 15),
+          //     child: Container(
+          //         decoration: BoxDecoration(
+          //             color: const Color(0xffEDEDED),
+          //             borderRadius: BorderRadius.circular(3)),
+          //         height: 10,
+          //         width: 100)),
+          readText(
+              text: 'Контакти фермера',
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+              size: 22,
+              padding: const EdgeInsets.only(top: 25, bottom: 30)),
           orderInfo(header: 'ФІО', text: modelOrder.userName.toString()),
           orderInfo(header: 'Регіон', text: modelOrder.userRegion.toString()),
           orderInfo(header: 'Район', text: modelOrder.userDistrict.toString()),
           orderInfo(header: 'Місто', text: modelOrder.userCity.toString()),
-          orderInfo(header: 'E-mail', text: modelOrder.userEmail.toString(), onPressed: () => launchUrl(Uri(scheme: 'mailto', path: modelOrder.userEmail.toString()))),
-          orderInfo(header: 'Телефон', text: modelOrder.userPhone.toString(), onPressed: () => launchUrl(Uri.parse(modelOrder.userPhone.toString()))),
-          bStyle(
-              padding: const EdgeInsets.only(left: 15, right: 15, top: 20),
-              text: 'Закрити',
-              size: 23,
-              c: c,
-              colorText: Colors.black,
-              vertical: 15,
-              colorButt: const Color(0xffF2F2F2),
-              onPressed: () => Navigator.pop(c))
+          orderInfo(
+              header: 'E-mail',
+              text: modelOrder.userEmail.toString(),
+              onPressed: () => launchUrl(Uri(
+                  scheme: 'mailto', path: modelOrder.userEmail.toString()))),
+          orderInfo(
+              header: 'Телефон',
+              text: modelOrder.userPhone.toString(),
+              onPressed: () async {
+                final phoneNumber = Uri(scheme: 'tel', path: '${modelOrder.userPhone}');
+                if (await canLaunchUrl(phoneNumber)) {
+                  await launchUrl(phoneNumber,
+                      mode: LaunchMode.externalApplication);
+                } else {
+                  log('Could not launch $phoneNumber');
+                }
+              }),
+          // bStyle(
+          //     padding: const EdgeInsets.only(left: 15, right: 15, top: 20),
+          //     text: 'Закрити',
+          //     size: 23,
+          //     c: c,
+          //     colorText: Colors.black,
+          //     vertical: 15,
+          //     colorButt: const Color(0xffF2F2F2),
+          //     onPressed: () => Navigator.pop(c))
         ],
       ),
     );
   }
 
-  Widget orderInfo({required String header, required String text, Function()? onPressed}) => Padding(
-      padding: const EdgeInsets.only(bottom: 10, left: 15, right: 15),
-      child: BTransparentScalableButton(
-          onPressed: onPressed ?? () {},
-          scale: onPressed == null ? ScaleFormat.none : ScaleFormat.small,
-          child: Row(children: [
-            readText(text: header, color: const Color(0xffA9A9A9), size: 20),
-            Expanded(child: readText(text: text, color: Colors.black, size: 20, fontWeight: FontWeight.w500, align: TextAlign.end))
-          ])));
+  Widget orderInfo(
+          {required String header,
+          required String text,
+          Function()? onPressed}) =>
+      Padding(
+          padding: const EdgeInsets.only(bottom: 10, left: 15, right: 15),
+          child: BTransparentScalableButton(
+              onPressed: onPressed ?? () {},
+              scale: onPressed == null ? ScaleFormat.none : ScaleFormat.small,
+              child: Row(children: [
+                readText(
+                    text: header, color: const Color(0xffA9A9A9), size: 20),
+                Expanded(
+                    child: readText(
+                        text: text,
+                        color: Colors.black,
+                        size: 20,
+                        fontWeight: FontWeight.w500,
+                        align: TextAlign.end))
+              ])));
 }
--- lib/app/home/order/order_screen.dart
+++ lib/app/home/order/order_screen.dart
@@ -1,3 +1,4 @@
+import 'package:agro/app/home/order/components/contact.dart';
 import 'package:agro/model/model_order_price/model_order_price.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -19,6 +20,7 @@ class OrderScreen extends StatefulWidget {
 
 class _OrderScreenState extends State<OrderScreen> {
   OrderController controller = Get.find();
+  bool contactOpened = false;
 
   @override
   void initState() {
@@ -30,83 +32,143 @@ class _OrderScreenState extends State<OrderScreen> {
   Widget build(BuildContext c) {
     return BlockPageScreen(
         headerSize: 20,
-        header: controller.loadPage ? 'Заявка №${controller.modelOrder.id} від ${DateFormat('dd.MM.yyyy').format(controller.modelOrder.startDate!)}' : 'Ваші заявки',
+        header: controller.loadPage
+            ? 'Заявка №${controller.modelOrder.id} від ${DateFormat('dd.MM.yyyy').format(controller.modelOrder.startDate!)}'
+            : 'Ваші заявки',
         isBack: true,
         theme: SystemUiOverlayStyle.dark,
         onPressedReturn: controller.onBack,
         child: !controller.loadPage
             ? const SizedBox()
-            : Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                const SizedBox(height: 30),
-                orderInfo(header: 'Область', text: controller.modelOrder.region.toString()),
-                orderInfo(header: 'Культура', text: controller.modelOrder.crop.toString()),
-                orderInfo(header: 'Об’єм', text: controller.modelOrder.capacity.toString()),
-                orderInfo(header: 'Рік врожаю', text: controller.modelOrder.harvestYear.toString()),
-                orderInfo(header: 'Форма оплати', text: controller.modelOrder.payment.toString()),
-                orderInfo(header: 'Тип доставки', text: controller.modelOrder.deliveryForm.toString()),
-                if (controller.modelOrder.comment != null && controller.modelOrder.comment != '') ...[
-                  readText(text: 'Коментар', color: const Color(0xffA9A9A9), size: 20),
-                  readText(text: controller.modelOrder.comment!, color: Colors.black, size: 20, fontWeight: FontWeight.w500, padding: const EdgeInsets.only(top: 10))
-                ],
-                if (controller.modelUser.role == 'distrib') ...[
-                  Padding(
-                      padding: const EdgeInsets.only(top: 35),
-                      child: Row(children: [
-                        Expanded(
-                            child: bStyle(
-                                width: 160,
-                                text: 'Контакти',
-                                size: 23,
-                                c: c,
-                                colorText: Colors.black,
-                                vertical: 15,
-                                colorButt: const Color(0xffF2F2F2),
-                                onPressed: controller.onContact)),
-                        const SizedBox(width: 25),
-                        Expanded(
-                            child: bStyle(
-                                width: 160,
-                                text: 'Відгуки',
-                                size: 23,
-                                c: c,
-                                colorText: Colors.black,
-                                vertical: 15,
-                                colorButt: const Color(0xffF2F2F2),
-                                onPressed: controller.onReview))
-                      ])),
-                  const SizedBox(height: 25),
-                  bStyle(key: UniqueKey(), text: 'Додати ціну', size: 23, c: c, colorButt: const Color(0xffFCD300), onPressed: controller.onAddPrice)
-                ] else ...[
-                  bStyle(
-                      key: UniqueKey(),
-                      padding: const EdgeInsets.only(top: 35),
-                      text: DateTime.now().millisecondsSinceEpoch > controller.modelOrder.endDate!.millisecondsSinceEpoch ? 'Продано' : 'Продав',
-                      size: 23,
-                      c: c,
-                      active: DateTime.now().millisecondsSinceEpoch < controller.modelOrder.endDate!.millisecondsSinceEpoch,
-                      colorButt: DateTime.now().millisecondsSinceEpoch > controller.modelOrder.endDate!.millisecondsSinceEpoch
-                          ? const Color(0xffF2F2F2)
-                          : const Color(0xffFCD300),
-                      onPressed: controller.onSell)
-                ],
-                Row(children: [
-                  readText(text: 'Запропоновані ціни', color: Colors.black, size: 20, fontWeight: FontWeight.w600, padding: const EdgeInsets.only(top: 25)),
-                  const Spacer(),
-                  readText(
-                      text: controller.totalPrice.toString(),
-                      color: const Color(0xffA9A9A9),
-                      size: 22,
-                      fontWeight: FontWeight.w600,
-                      padding: const EdgeInsets.only(top: 25))
-                ]),
-                for (ModelOrderPrice i in controller.modelOrderPrice) ...[BlockPriceOrder(modelOrderPrice: i)]
-              ]));
+            : SingleChildScrollView(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                    const SizedBox(height: 30),
+                    orderInfo(
+                        header: 'Область',
+                        text: controller.modelOrder.region.toString()),
+                    orderInfo(
+                        header: 'Культура',
+                        text: controller.modelOrder.crop.toString()),
+                    orderInfo(
+                        header: 'Об’єм',
+                        text: controller.modelOrder.capacity.toString()),
+                    orderInfo(
+                        header: 'Рік врожаю',
+                        text: controller.modelOrder.harvestYear.toString()),
+                    orderInfo(
+                        header: 'Форма оплати',
+                        text: controller.modelOrder.payment.toString()),
+                    orderInfo(
+                        header: 'Тип доставки',
+                        text: controller.modelOrder.deliveryForm.toString()),
+                    if (controller.modelOrder.comment != null &&
+                        controller.modelOrder.comment != '') ...[
+                      readText(
+                          text: 'Коментар',
+                          color: const Color(0xffA9A9A9),
+                          size: 20),
+                      readText(
+                          text: controller.modelOrder.comment!,
+                          color: Colors.black,
+                          size: 20,
+                          fontWeight: FontWeight.w500,
+                          padding: const EdgeInsets.only(top: 10))
+                    ],
+                    contactOpened
+                        ? ContactScreen(modelOrder: controller.modelOrder)
+                        : const SizedBox(),
+                    if (controller.modelUser.role == 'distrib') ...[
+                      Padding(
+                          padding: const EdgeInsets.only(top: 35),
+                          child: Row(children: [
+                            Expanded(
+                                child: bStyle(
+                                    width: 160,
+                                    text: contactOpened ? "Сховати" : 'Контакти',
+                                    size: 23,
+                                    c: c,
+                                    colorText: Colors.black,
+                                    vertical: 15,
+                                    colorButt: const Color(0xffF2F2F2),
+                                    onPressed: () {
+                                      setState(() {
+                                        contactOpened = !contactOpened;
+                                      });
+                                    })),
+                            const SizedBox(width: 25),
+                            Expanded(
+                                child: bStyle(
+                                    width: 160,
+                                    text: 'Відгуки',
+                                    size: 23,
+                                    c: c,
+                                    colorText: Colors.black,
+                                    vertical: 15,
+                                    colorButt: const Color(0xffF2F2F2),
+                                    onPressed: controller.onReview))
+                          ])),
+                      const SizedBox(height: 25),
+                      bStyle(
+                          key: UniqueKey(),
+                          text: 'Додати ціну',
+                          size: 23,
+                          c: c,
+                          colorButt: const Color(0xffFCD300),
+                          onPressed: controller.onAddPrice)
+                    ] else ...[
+                      bStyle(
+                          key: UniqueKey(),
+                          padding: const EdgeInsets.only(top: 35),
+                          text: DateTime.now().millisecondsSinceEpoch >
+                                  controller.modelOrder.endDate!
+                                      .millisecondsSinceEpoch
+                              ? 'Продано'
+                              : 'Продав',
+                          size: 23,
+                          c: c,
+                          active: DateTime.now().millisecondsSinceEpoch <
+                              controller
+                                  .modelOrder.endDate!.millisecondsSinceEpoch,
+                          colorButt: DateTime.now().millisecondsSinceEpoch >
+                                  controller.modelOrder.endDate!
+                                      .millisecondsSinceEpoch
+                              ? const Color(0xffF2F2F2)
+                              : const Color(0xffFCD300),
+                          onPressed: controller.onSell)
+                    ],
+                    Row(children: [
+                      readText(
+                          text: 'Запропоновані ціни',
+                          color: Colors.black,
+                          size: 20,
+                          fontWeight: FontWeight.w600,
+                          padding: const EdgeInsets.only(top: 25)),
+                      const Spacer(),
+                      readText(
+                          text: controller.totalPrice.toString(),
+                          color: const Color(0xffA9A9A9),
+                          size: 22,
+                          fontWeight: FontWeight.w600,
+                          padding: const EdgeInsets.only(top: 25))
+                    ]),
+                    for (ModelOrderPrice i in controller.modelOrderPrice) ...[
+                      BlockPriceOrder(modelOrderPrice: i)
+                    ]
+                  ])));
   }
 
   Widget orderInfo({required String header, required String text}) => Padding(
       padding: const EdgeInsets.only(bottom: 10),
       child: Row(children: [
         readText(text: header, color: const Color(0xffA9A9A9), size: 20),
-        Expanded(child: readText(text: text, color: Colors.black, size: 20, fontWeight: FontWeight.w500, align: TextAlign.end))
+        Expanded(
+            child: readText(
+                text: text,
+                color: Colors.black,
+                size: 20,
+                fontWeight: FontWeight.w500,
+                align: TextAlign.end))
       ]));
 }
